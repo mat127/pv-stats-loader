9fa7789547c98694cabcf5bdcd2aa978945beafb
mat127 <mat127@seznam.cz>
2023-10-29 22:29:09 +0100

    [ref] remove build directory

--- build/main.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const db_1 = require("./db");
-const ws_1 = require("./ws");
-(0, ws_1.getCurve)(new Date(2023, 9, 26))
-    .then(response => db_1.db.load(response.data.curve))
-    .catch(error => console.log(error));
