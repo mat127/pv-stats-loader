6f0b6215bd77bb0883a762c0c89cb5d9d5796bc3
mat127 <mat127@seznam.cz>
2023-10-29 22:35:51 +0100

    [ref] read Authorization header from WS_AUTH env variable

--- src/ws.ts
+++ src/ws.ts
@@ -19,6 +19,10 @@ export interface StationCurve {
 const wattsonicCloudUrl = 'https://www.wattsonic.cloud/api/sys/curve/station/queryStationCurve';
 const stationId = '1699670261225848834';
 
+if(!process.env.WS_AUTH) {
+    throw new Error('WS_AUTH not defined');
+}
+
 export async function getCurve(date?: Date): Promise<StationCurve> {
     const _date = date ?? new Date();
     const payload = {
@@ -32,7 +36,7 @@ export async function getCurve(date?: Date): Promise<StationCurve> {
     return axios.post(wattsonicCloudUrl, payload, {
         headers: {
             'Content-type': 'application/json; charset=UTF-8',
-            Authorization: 'eyJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE2OTg2MDcyMDcsIlVfSUQiOiIxNjk5NjcwMjYxMjIxNjU0NTMwIiwiSVNfQURNSU4iOnRydWUsIk9SR19JRCI6IjE2OTk2NzAyNjEyMTc0NjAyMjUiLCJDUkVBVEVfVElNRSI6MTY5ODYwNzIwNzA0Mn0.b__mQhzyKolEEsRdTGYJLDI-8lUZ9aGWOXStIaN-B4A'
+            Authorization: process.env.WS_AUTH
         }
     })
         .then(response => response.data);
