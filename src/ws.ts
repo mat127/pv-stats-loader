78475037dee8fa665369462fe8a2953d66828b50
mat127 <mat127@seznam.cz>
2023-11-04 22:34:36 +0100

    [feat] add login support

--- src/ws.ts
+++ src/ws.ts
@@ -16,26 +16,47 @@ export interface StationCurve {
     }
 }
 
-const wattsonicCloudUrl = 'https://www.wattsonic.cloud/api/sys/curve/station/queryStationCurve';
-const stationId = '1699670261225848834';
-
-if(!process.env.WS_AUTH) {
-    throw new Error('WS_AUTH not defined');
+const wattsonicCloudUrl = 'https://www.wattsonic.cloud/api/sys';
+if(!process.env.STATION_ID) {
+    throw new Error("STATION_ID not defined");
 }
+if(!process.env.WS_LOGIN_PAYLOAD) {
+    throw new Error("WS_LOGIN_PAYLOAD not defined");
+}
+const token = {
+    value: '',
+    get: async function(): Promise<string> {
+        if (!this.value) {
+            this.value = await this.load();
+        }
+        return this.value!;
+    },
+    load: async function(): Promise<string> {
+        return axios.post(
+            `${wattsonicCloudUrl}/login/manager`,
+            process.env.WS_LOGIN_PAYLOAD,
+            {
+                headers: { 'Content-type': 'application/json; charset=UTF-8' }
+            }
+        )
+            .then(response => response.data.data.token);
+    }
+};
 
 export async function getCurve(date: Date): Promise<StationCurve> {
     const payload = {
         date: date.toISOString().split('T')[0],
         durationType: 1,
-        stationId,
+        stationId: process.env.STATION_ID,
         stationType: 0,
         timeZoneOffset: -date.getTimezoneOffset(), // to get correct dateStamp values
         type: 'update'
     };
-    return axios.post(wattsonicCloudUrl, payload, {
+    const auth = await token.get();
+    return axios.post(`${wattsonicCloudUrl}/curve/station/queryStationCurve`, payload, {
         headers: {
             'Content-type': 'application/json; charset=UTF-8',
-            Authorization: process.env.WS_AUTH
+            Authorization: auth
         }
     })
         .then(response => response.data);
